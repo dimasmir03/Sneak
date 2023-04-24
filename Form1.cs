c04d7e15dfb9dced8f5330062722770c1019810d
dimasmir03 <saenkodmitriiol@gmail.com>
2023-04-24 23:51:26 +0300

    Revert "wad"
    This reverts commit 46f6ffac9fd754ca4de544b2464ff4c95fcdc912.

--- Form1.cs
+++ Form1.cs
@@ -14,6 +14,7 @@ namespace Курсовая
     public partial class Form1 : Form
     {
         Graphics g;
+        int w, h;
         public Form1()
         {
             InitializeComponent();
@@ -22,24 +23,33 @@ namespace Курсовая
             Height = 480;
 
             Height = (Height / 20) * 20;
-            Width = Width / 20 * 20 + 50;
+            Width = (Width / 20 * 20);
             
             g = CreateGraphics();
+            w=Width; h=Height;
         }
 
         public void drawGrid()
         {
             Console.WriteLine(Width);
-            //Height = (Height / 20) * 20;
-            //Width = (Width / 20) * 20;
+            Height = (Height / 20) * 20;
+            Width = (Width / 20) * 20;
             g = CreateGraphics();
             for (var i = 0; i < Height/20; i++)
             {
                 for (var j = 0; j < Width/20; j++)
                 {
+                    //fillRect(i, j, CELL_SIZE, CELL_SIZE)
                     g.DrawRectangle(new Pen(Color.Red,1), j*20, i*20, 20, 20);
                 }
             }
+            //g.FillRectangle(new SolidBrush(Color.Red),0,0,w,h);
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+
+            //this.OnPaint(null);
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
